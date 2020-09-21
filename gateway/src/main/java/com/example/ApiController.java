3bea752a0a2f82038e807b6db420af85ff04ed1a
Iván López <lopez.ivan@gmail.com>
2020-09-21 23:37:23 +0200

    Add Zipkin

--- gateway/src/main/java/com/example/ApiController.java
+++ gateway/src/main/java/com/example/ApiController.java
@@ -4,6 +4,8 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
+import io.micronaut.tracing.annotation.NewSpan;
+import io.micronaut.tracing.annotation.SpanTag;
 
 import io.reactivex.Single;
 
@@ -17,8 +19,9 @@ public class ApiController implements EncryptOperations {
         this.encryptClient = encryptClient;
     }
 
+    @NewSpan
     @Get("/encrypt/{text}")
-    public Single<MyMessage> encrypt(String text) {
+    public Single<MyMessage> encrypt(@SpanTag("plain-text") String text) {
         return encryptClient.encrypt(text);
     }
 }
